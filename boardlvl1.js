a46a62bfcbaf5f7e517d006351edf0946d06753f
elizabith.costey <costey.ea@gmail.com>
2018-07-03 14:53:57 -0400

    tile divs now have token classes, but 'tile' class was disabled. Need to fix this.

--- boardlvl1.js
+++ boardlvl1.js
@@ -1,24 +1,25 @@
 function makeBoard() {
     const arrTiles = [
-        [0, 0, 0, 'goblin', 0, 'exit'],
-        ['goblin', 0, 'treasure', 0, 0, 0],
-        [0, 0, 0, 0, 0, 0],
-        ['treasure', 'goblin', 0, 0, 0, 'goblin'],
-        [0, 0, 0, 0, 0, 0],
-        [0, 'start', 0, 0, 0, 0]
+        ['O', 'O', 'O', 'goblin', 'O', 'exit'],
+        ['goblin', 'O', 'treasure', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', 'O'],
+        ['treasure', 'goblin', 'O', 'O', 'O', 'goblin'],
+        ['O', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'start', 'O', 'O', 'O', 'O']
     ];
     const board1 = document.getElementById('board1');
     for (let i = 0; i < arrTiles.length; i += 1) {
         for (let j = 0; j < arrTiles.length; j += 1) {
             const tile = document.createElement('div');
             tile.className = 'tile';
+            const X = addTileClass(tile, arrTiles[i][j]);
+            tile.className = X;
             board1.appendChild(tile);
          }
     }
-
 }
 makeBoard ();
 
-function addTileClass () {
-
+function addTileClass (element , cName){
+    return(element.className = cName);
 }
