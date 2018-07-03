8c22f1306be85823bcc04bbaf25931958bdfbd0e
elizabith.costey <costey.ea@gmail.com>
2018-07-03 12:26:57 -0400

    Made board1, still working on adding monster classNames to tile divs.

--- boardlvl1.js
+++ boardlvl1.js
@@ -1,21 +1,24 @@
 function makeBoard() {
     const arrTiles = [
-        [0, 0, 0, 'goblin', 'exit'],
-        ['goblin', 0, 'treasure', 0, 0],
-        [0, 0, 0, 0, 0],
-        ['treasure', 'goblin', 0, 0, 'goblin'],
-        [0, 0, 0, 0, 0],
-        [0, 'start', 0, 0, 0],
+        [0, 0, 0, 'goblin', 0, 'exit'],
+        ['goblin', 0, 'treasure', 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        ['treasure', 'goblin', 0, 0, 0, 'goblin'],
+        [0, 0, 0, 0, 0, 0],
+        [0, 'start', 0, 0, 0, 0]
     ];
     const board1 = document.getElementById('board1');
-    arrTilesLength = arrTiles.length;
-    for (i = 0; i < arrTilesLength; i += 1) {
-        for (j = 0; j < arrTilesLength; j += 1) {
-            const tile = document.createElement("div");
+    for (let i = 0; i < arrTiles.length; i += 1) {
+        for (let j = 0; j < arrTiles.length; j += 1) {
+            const tile = document.createElement('div');
+            tile.className = 'tile';
             board1.appendChild(tile);
-            text(arrTilesLength[i]).appendTo('board1');
-        }
+         }
     }
 
 }
 makeBoard ();
+
+function addTileClass () {
+
+}
